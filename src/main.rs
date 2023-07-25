409fb9085751a50bce830fa0421040d5e1784c88
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-07-25 19:51:44 -0300

    feat: works when some process is running on port
    It works, kills the process running on given port, when there is a
    process running.
    
    When there is no process it returns a error(??)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let kill_arg = std::env::args().position(|r| r == "--kill" || r == "-k");
+
+    if let Some(kill_arg) = kill_arg {
+        let kill_arg = kill_arg + 1;
+        let kill_arg = std::env::args().nth(kill_arg).unwrap();
+        let kill_arg = kill_arg.parse::<u16>().unwrap();
+
+        let output = std::process::Command::new("lsof")
+            .arg("-t")
+            .arg("-i")
+            .arg(format!(":{}", kill_arg))
+            .output()
+            .expect("failed to execute process");
+
+        let output = String::from_utf8_lossy(&output.stdout);
+        let output = output.split_whitespace().collect::<Vec<&str>>();
+        let pid = output.get(0).unwrap_or(&"0");
+
+        std::process::Command::new("kill")
+            .arg(format!("{}", pid))
+            .output()
+            .expect("failed to execute process");
+    } else {
+        println!("Portter: Need a port to kill!");
+    }
 }
